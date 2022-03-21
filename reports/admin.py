6c40ccc5095ac658c375286b185bd192e1c20f33
InesSC15 <ines@panoimagen.com>
2022-03-21 11:55:10 +0100

    Add models to admin

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Defibrillator, Company, Professional, Client, Patch
 
-# Register your models here.
+admin.site.register(Company)
+admin.site.register(Client)
+admin.site.register(Professional)
+admin.site.register(Patch)
+admin.site.register(Defibrillator)
