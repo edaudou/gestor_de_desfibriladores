9b409bd7010ef69d843ebfa55f79c4d258cb8f05
InesSC15 <ines@panoimagen.com>
2022-03-21 12:47:40 +0100

    Only required phone, nif and cif

--- reports/models.py
+++ reports/models.py
@@ -1,17 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Contact(models.Model):
-    address = models.CharField(verbose_name='Dirección', max_length=255,) 
-    email = models.CharField(verbose_name='E-mail', max_length=100,) 
+    address = models.CharField(verbose_name='Dirección', max_length=255, null = True)
+    email = models.CharField(verbose_name='E-mail', max_length=100, null = True)
     phone = models.CharField(verbose_name='Teléfono', max_length=9,)
     class Meta:
         abstract = True
 
 class Person(Contact):
     id = models.AutoField(primary_key=True)
-    first_name = models.CharField(verbose_name='Nombre',max_length=100)
-    last_name = models.CharField(verbose_name='Apellidos',max_length=255)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null = True, blank=True)
+    first_name = models.CharField(verbose_name='Nombre',max_length=100, null = True, blank=True)
+    last_name = models.CharField(verbose_name='Apellidos',max_length=255, null = True, blank=True)
     nif = models.CharField(verbose_name='NIF', max_length=9,)
+    created_at = models.DateTimeField(auto_now_add=True, null = True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null = True, blank=True)
+
     class Meta:
         abstract = True
     def save(self, *args, **kwargs):
@@ -20,7 +25,7 @@ class Person(Contact):
 
 class Company(Contact):
     id = models.AutoField(verbose_name='ID', primary_key=True)
-    cif = models.CharField(verbose_name='Teléfono', max_length=9,)
+    cif = models.CharField(verbose_name='cif', max_length=9,)
     def save(self, *args, **kwargs):
         self.product_type = 'Contact'
         return super(Contact, self).save(*args, **kwargs)
@@ -38,21 +43,24 @@ class Client(Person):
 
 class Patch(models.Model):
     id = models.AutoField(verbose_name='ID', primary_key=True)
-    expiration_date =  models.DateTimeField(verbose_name='Fecha de expiración',)
+    expiration_date =  models.DateTimeField(verbose_name='Fecha de expiración', null = True, blank=True)
 
 class Defibrillator(models.Model):
     id = models.AutoField(verbose_name='ID', primary_key=True)
     # Nombre del modelo
-    model = models.CharField(verbose_name='Modelo', max_length=100,)
-    brand = models.CharField(verbose_name='Marca', max_length=100,)
-    location = models.CharField(verbose_name='Localización', max_length=255,)
+    model = models.CharField(verbose_name='Modelo', max_length=100, null = True, blank=True)
+    brand = models.CharField(verbose_name='Marca', max_length=100, null = True, blank=True)
+    location = models.CharField(verbose_name='Localización', max_length=255, null = True, blank=True)
     # Propietario -> cliente? user? sin mas un nombre de una empresa?
     client = models.ForeignKey(Client,  on_delete=models.CASCADE, related_name="%(class)s")
-    installation_date = models.DateTimeField()
+    installation_date = models.DateTimeField( null = True, blank=True)
     # Fecha caducidad del parche o parches -> otro model? -> habrá que crear un objeto parche que tenga atributo fecha de caducidad para reflejarlo aquí
-    patch = models.ForeignKey(Patch,  on_delete=models.CASCADE, related_name="%(class)s")
+    patch = models.ForeignKey(Patch,  on_delete=models.CASCADE, related_name="%(class)s", null = True, blank=True)
     # Fecha caducidad de la bateria
-    battery_expiration_date = models.DateTimeField()
-    check_date = models.DateTimeField()
-    professional = models.ManyToManyField(Professional, related_name="%(class)s")
+    battery_expiration_date = models.DateTimeField(null = True, blank=True)
+    check_date = models.DateTimeField(null = True, blank=True)
+    professional = models.ManyToManyField(Professional, related_name="%(class)s", blank=True)
+    #Para almacenar en postgis y luego representarlo en la api de google maps
+    # longitude  = ...
+    # latitude = ...
 
